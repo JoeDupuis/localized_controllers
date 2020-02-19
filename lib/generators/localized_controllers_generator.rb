ce0f933d6def8b3ef19b21afd65d7d96c455b0fc
Joe Dupuis <joe.dupuis@xylio.net>
2020-02-18 20:07:28 -0500

    Pluralize

--- lib/generators/localized_controllers_generator.rb
+++ lib/generators/localized_controllers_generator.rb
@@ -1,7 +1,7 @@
-class LocalizedControllerGenerator < Rails::Generators::Base
+class LocalizedControllersGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
 
-  def create_localized_controller_file
+  def create_localized_controllers_file
     copy_file "controller.rb.tt", "lib/templates/rails/scaffold_controller/controller.rb.tt"
   end
 end
